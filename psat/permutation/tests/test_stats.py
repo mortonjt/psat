3c3d0d67059f412a9f04af53c2c7adc772b4bbd1
Jamie Morton <jamietmorton@gmail.com>
2015-03-30 14:34:13 -0600

    STY: Fixed some pep8 errors

--- psat/permutation/tests/test_stats.py
+++ psat/permutation/tests/test_stats.py
@@ -10,14 +10,14 @@ class TestStats(unittest.TestCase):
         mat = np.array(
             np.matrix(np.vstack((
                 np.hstack(np.random.random(N/2),
-                          np.random.random(N/2)+100)
+                          np.random.random(N/2) + 100)
                 np.random.random(N),
                 np.random.random(N),
                 np.random.random(N),
-                np.random.random(N),dtype=np.float32))
+                np.random.random(N), dtype=np.float32))
 
-        cats = np.array([0]*(N/2)+[1]*(N/2),dtype=np.float32)
-        t_stats, pvalues = ttest(mat,cats,1000)
+        cats = np.array([0] * (N/2) + [1] * (N/2), dtype=np.float32)
+        t_stats, pvalues = ttest(mat, cats, 1000)
 
 
 if __name__=="__main__":
