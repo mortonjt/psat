ba9e2bde57a2d102cb2f0088eeecccf41c2a97ee
Jamie Morton <jamietmorton@gmail.com>
2015-05-03 15:49:39 -0600

    DEV: Adding in f-oneway

--- psat/permutation/stats.py
+++ psat/permutation/stats.py
@@ -1,9 +1,11 @@
+from __future__ import division
+from future.builtins import range
 import numpy as np
 import copy
 from psat.util import check_random_state
 
 
-def ttest(mat, cats, axis=0, permutations=10000,
+def permutation_ttest(mat, cats, axis=0, permutations=10000,
           chunk_size=10, equal_var=True, random_state=None):
     """
     Calculates the T-test for the means of TWO INDEPENDENT samples of
@@ -35,10 +37,8 @@ def ttest(mat, cats, axis=0, permutations=10000,
 
     Returns
     -------
-    t : float or array
+    t-stat : float or array
         The calculated t-statistic.
-    prob : float or array
-        The two-tailed p-value.
     """
     if axis == 0:
         mat = mat.transpose()
@@ -78,8 +78,81 @@ def ttest(mat, cats, axis=0, permutations=10000,
     cmps = abs(t_stat[:, 1:].transpose()) >= abs(t_stat[:, 0])
     pvalues = (cmps.sum(axis=0) + 1.) / (permutations + 1.)
 
-    return t_stat[:, 0], pvalues
+    return t_stat
 
+def permutation_f_oneway(mat, cats, **kwds):
+    """
+    Performs a 1-way ANOVA.
+
+    The one-way ANOVA tests the null hypothesis that two or more groups have
+    the same population mean.  The test is applied to samples from two or
+    more groups, possibly with differing sizes.
+
+    Parameters
+    ----------
+    mat : np.ndarray
+        Contingency table
+    cats : array_like
+        Categorical vector
+    axis : int, optional
+        Axis to conduct permutation test on.
+    permutations : int, optional
+        If permutations > 0, then a permutation test will be conducted to
+        calculate the p-values
+    random_state : int or RandomState
+        Pseudo number generator state used for random sampling.
+
+    Returns
+    -------
+    F-value : float
+        The computed F-values of the test.
+    """
+
+    params = {'permutations':10000, 'random_state':0, 'axis':0}
+    for key in ('permutations', 'random_state', 'axis'):
+        params[key] = kwds.get(key, params[key])
+    permutations = params['permutations']
+    random_state = params['random_state']
+    axis = params['axis']
+
+    random_state = check_random_state(random_state)
+    if axis == 0:
+        mat = mat.transpose()
+    if len(mat.shape) < 2:
+        mat = mat.reshape((1, len(mat)))
+    r, c = mat.shape
+    num_cats = len(np.unique(cats))
+    f_stat = np.zeros((r, num_cats*(permutations+1)))
+    copy_cats = copy.deepcopy(cats)
+
+    perms = _init_summation_index(copy_cats, permutations)
+    n_samp, c = perms.shape
+    mat2 = np.multiply(mat, mat)
+    S = mat.sum(axis=1)
+    SS = mat2.sum(axis=1)
+    sstot = SS - np.multiply(S,S) / n_samp
+    sstot = sstot.reshape((len(sstot),1))
+    # Create index to sum the ssE together
+    sum_groups = np.arange(num_cats, dtype=np.int32) // num_cats
+    _sum_idx = _init_summation_index(sum_groups)
+
+    # Perform matrix multiplication on data matrix
+    # and calculate sums and squared sums and sum of squares
+    _sums = np.dot(mat, perms)
+    _sums2 = np.dot(np.multiply(mat, mat), perms)
+
+    tot = perms.sum(axis=0)
+    ss = _sums2 - np.multiply(_sums, _sums)/tot
+    sserr = np.dot(ss, _sum_idx)
+    sstrt = sstot - sserr
+    dftrt = num_cats - 1
+    dferr = np.dot(tot, _sum_idx) - num_cats
+    f_stat = np.ravel((sstrt / dftrt) / (sserr / dferr))
+
+    cmps = f_stat[:, 1:].transpose() >= f_stat[:, 0]
+    pvalues = (cmps.sum(axis=0) + 1.) / (permutations + 1.)
+
+    return f_stat
 
 #################################################################
 #                       Helper functions                        #
@@ -97,8 +170,8 @@ def _init_summation_index(cats, permutations, random_state=None):
     copy_cats = copy.deepcopy(cats)
     perms = np.array(np.zeros((c, num_cats*(permutations+1)),
                               dtype=cats.dtype))
-    for m in range(permutations+1):
-        for i in range(num_cats):
+    for m in xrange(permutations+1):
+        for i in xrange(num_cats):
             perms[:, num_cats*m+i] = (copy_cats == i).astype(cats.dtype)
         random_state.shuffle(copy_cats)
     return perms
